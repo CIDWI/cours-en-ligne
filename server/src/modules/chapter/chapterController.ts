57add059b548bd3c2ada2cd1dcbda033d581cb16
BenjaminZaoui <benjaminzaouipro@gmail.com>
2025-06-11 11:03:14 +0200

    correctifs/upgrade de chapterController.ts sue l

--- server/src/modules/chapter/chapterController.ts
+++ server/src/modules/chapter/chapterController.ts
@@ -1,12 +1,11 @@
 import 'dotenv/config';
-import { Router } from 'express';
-import { chapterRepository } from './chapterRepository';
-import { createValidator } from 'express-joi-validation';
+import {Router} from 'express';
+import {chapterRepository} from './chapterRepository';
+import {createValidator} from 'express-joi-validation';
 import Joi from 'joi';
-import {
-    expressjwt,
-    Request as JWTRequest,
-} from 'express-jwt';
+import {expressjwt, Request as JWTRequest,} from 'express-jwt';
+import {courseRepository} from "../cours/courseRepository";
+
 
 export const chapterController = Router();
 const validator = createValidator();
@@ -22,6 +21,7 @@ chapterController.use(
 // Joi Schemas
 const createChapterSchema = Joi.object({
     title: Joi.string().required(),
+    courseId: Joi.number().optional(),
 });
 
 const getChapterSchema = Joi.object({
@@ -30,12 +30,13 @@ const getChapterSchema = Joi.object({
 
 const updateChapterSchema = Joi.object({
     title: Joi.string().optional(),
+    courseId: Joi.number().optional(),
 });
 
 // GET all courses
 chapterController.get('/', async (req: JWTRequest, res) => {
 
-    res.send(await chapterRepository.find({ relations: ['lessons'] }));
+    res.send(await chapterRepository.find({relations: ['lessons']}));
 
 });
 
@@ -43,15 +44,14 @@ chapterController.get('/', async (req: JWTRequest, res) => {
 chapterController.get('/:id', validator.params(getChapterSchema), async (req: JWTRequest, res) => {
         const id = Number(req.params.id);
         const chapter = await chapterRepository.findOne({
-            where: { id },
+            where: {id},
             relations: ['lessons'],
         });
         if (chapter) {
             res.send(chapter);
 
-        }
-        else {
-            res.status(404).send({ error: 'Chapter not found' });
+        } else {
+            res.status(404).send({error: 'Chapter not found'});
         }
     },
 );
@@ -62,47 +62,67 @@ chapterController.post('/', validator.body(createChapterSchema), async (req: JWT
         try {
 
             if (req.auth?.role === 'admin') {
-                const chapter = await chapterRepository.save({ title: req.body.title });
+                const course = await courseRepository.findOne({where: {id: req.body.courseId}, relations: ['chapters']});
+                let chapter;
+                if (req.body.courseId) {
+                    if (course) {
+                        chapter = await chapterRepository.save({
+                            title: req.body.title,
+                            course: course
+                        });
+                    } else {
+                        throw {status: 400, message: 'Course not found'};
+                    }
+                } else {
+                    chapter = await chapterRepository.save({
+                        title: req.body.title,
+                    });
+                }
+
+                
                 res.send(chapter);
-            }
-            else {
+            } else {
                 throw {status: 403, message: 'Forbidden'};
             }
 
-        }
-        catch (error: any) {
-            res.status(error.status).send({error: error.message});
+        } catch (error: any) {
+            res.status(error.status ?? 500).send({error: error.message ?? "internal server error"});
         }
     },
 );
 
 // UPDATE course (admin only)
-chapterController.put('/:id',validator.params(getChapterSchema), validator.body(updateChapterSchema), async (req: JWTRequest, res) => {
+chapterController.put('/:id', validator.params(getChapterSchema), validator.body(updateChapterSchema), async (req: JWTRequest, res) => {
         try {
             if (req.auth?.role === 'admin') {
                 const id = Number(req.params.id);
-                const chapter = await chapterRepository.findOneBy({ id });
+                const chapter = await chapterRepository.findOneBy({id});
+                const course = await courseRepository.findOne({where: {id: req.body.courseId}, relations: ['chapters']});
 
                 if (chapter) {
                     if (req.body.title) {
+                        if (req.body.courseId) {
+                            if (!course) {
+                                throw {status: 404, message: 'Course not found'};
+                            } else {
+                                chapter.course = course
+                            }
+                        }
                         chapter.title = req.body.title;
+
                         await chapterRepository.save(chapter);
                         res.send(chapter);
-                    }
-                    else {
-                        throw {status: 400, message: 'missing information to modify'};
+                    } else {
+                        throw {status: 400, message: 'Missing information to modify'};
                     }
 
-                }
-                else {
+                } else {
                     throw {status: 404, message: 'Chapter not found'};
                 }
-            }
-            else {
+            } else {
                 throw {status: 403, message: 'Forbidden'};
             }
-        }
-        catch (error: any) {
+        } catch (error: any) {
             res.status(error.status).send({error: error.message});
         }
     },
@@ -114,31 +134,27 @@ chapterController.delete('/:id', validator.params(getChapterSchema), async (req:
             if (req.auth?.role === 'admin') {
                 const id = Number(req.params.id);
                 const chapter = await chapterRepository.findOne({
-                    where: { id },
+                    where: {id},
                     relations: ['lessons'],
                 });
                 if (chapter) {
                     if (chapter.lessons.length === 0) {
-                        await chapterRepository.delete({ id });
+                        await chapterRepository.delete({id});
                         res.sendStatus(204);
-                    }
-                    else {
+                    } else {
                         throw {status: 400, message: 'Cannot delete chapter with lessons'};
                     }
 
-                }
-                else {
+                } else {
                     throw {status: 400, message: 'Chapter not found'};
                 }
 
 
-            }
-            else {
+            } else {
                 throw {status: 403, message: 'Forbidden'};
             }
 
-        }
-        catch (error: any) {
+        } catch (error: any) {
             res.status(error.status).send({error: error.message});
         }
 
