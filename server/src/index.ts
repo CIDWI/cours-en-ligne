853cf43b3a949a88c38625683be116e6e1fdcfba
BenjaminZaoui <benjaminzaouipro@gmail.com>
2025-06-13 15:27:22 +0200

    ajouts advancementController.ts fait

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import {courseController} from "./modules/cours/courseController";
 import {chapterController} from "./modules/chapter/chapterController";
 import {lessonController} from "./modules/lesson/lessonController";
 import {exerciseController} from "./modules/exercise/exerciseController";
+import {advancementController} from "./modules/advancement/advancementController";
 
 const app = express()
 
@@ -20,7 +21,7 @@ app.use('/course', courseController)
 app.use('/chapter', chapterController)
 app.use('/lesson', lessonController)
 app.use('/exercise', exerciseController)
-
+app.use('/advancement', advancementController)
 const port = process.env.PORT
     ? Number(process.env.PORT)
     : 3000
